dc6382e9b4347283e1d143e9251feea734f0d227
SALVADOR, 1 M. LIGAYAO <89230263+itsmenewbie03@users.noreply.github.com>
2022-04-16 13:59:25 +0800

    added printf for result

--- Major Activity 3.0 - C Strings/ReverseOrderNoSplit.c
+++ Major Activity 3.0 - C Strings/ReverseOrderNoSplit.c
@@ -6,6 +6,7 @@ int main()
     char string[100];
     printf("Enter any string: ");
     scanf("%[^\n]s", string);
+    printf("Reverse ordered words");
     int lastIndex = strlen(string) - 1;
     while (lastIndex >= 0)
     {
