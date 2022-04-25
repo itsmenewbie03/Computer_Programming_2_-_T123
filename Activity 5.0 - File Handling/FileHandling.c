c1f1787c1e32025b8ed3f00b8a960806a97ec4a6
Salvador Ligayao <futuremr.ligayao03@gmail.com>
2022-04-25 08:43:28 +0800

    uploading file handling act and another solution to reverse order

---
+++ Activity 5.0 - File Handling/FileHandling.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char fileLoc[100];
+    char *words[10][10];
+    FILE *file;
+    int lines;
+    printf("Input the file location: ");
+    scanf("%[^\n]s",fileLoc);
+    printf("Input How many lines to be appended: ");
+    scanf("%d",&lines);
+    file = fopen(fileLoc,"a+");
+    for(int i = 0; i<lines; i++){
+        char temp[100];
+        scanf("%s",temp);
+        words[i][0] = temp;
+        fprintf(file,"%s\n",words[i][0]);
+    }
+    fclose(file);
+}
